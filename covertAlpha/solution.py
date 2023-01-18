ab75a2631367352d5c01c9357a29665b2d3c5773
imtiazpy <ahmed.imti1994@gmail.com>
2023-01-18 22:10:50 +0600

    added enhanced solution for convert alpha

--- covertAlpha/solution.py
+++ covertAlpha/solution.py
@@ -2,16 +2,35 @@
 
 # This solution is not the shortest, Try solving it with less character
 
-s=input()
-n=''
+s = input()
+n = ''
 for i in s:
-    if i.isalpha():
-        if i.isupper():
-            n+=i
-            n+=i.lower()
-        else:
-            n+=i
-            n+=i.upper()
+  if i.isalpha():
+    if i.isupper():
+      n += i
+      n += i.lower()
     else:
-        n+=i
+      n += i
+      n += i.upper()
+  else:
+    n += i
 print(n)
+
+# Enhanced solution
+r = ''
+for i in input():
+  r += i
+  if i.isAlpha():
+    if i.isLower():
+      r += i.isUpper()
+    else:
+      r += i.isLower()
+print(r)
+
+
+# Another solution
+r=''
+for i in input():
+  r+=i
+  if i.isAlpha():r+=i.swapcase()
+print(r)
